f5a24b074c9845d5b04d6a45edbfcfdcd2637f95
lolguy91 <retek1544@gmail.com>
2023-09-12 18:18:09 +0200

    a little formatting

--- parsing/ini.c
+++ parsing/ini.c
@@ -1,53 +1,48 @@
 #ifdef HEAP_ACCESSABLE
 #include "ini.h"
+#include <arch/x86/essential.h>
 #include <types/string.h>
 #include <types/vector.h>
-#include <arch/x86/essential.h>
 #include <utils/heapdef.h>
 
-vector_static(struct key_value_pair,kvps);
+vector_static(struct key_value_pair, kvps);
 
-struct parsed_ini parse_ini(char* string)
-{
+struct parsed_ini parse_ini(char *string) {
     vector_init(&kvps);
     char currtag[255];
     uint8_t eqindex = 0;
-    char** splitstring = split(string,'\n');
-    for (size_t i = 0; splitstring[i]; i++)
-    {
-       if (splitstring[i][0] == '[')
-       {
-        for (int j = 1; j < strlen(splitstring[i]) - 1; j++)
-        {
-            if(splitstring[i][j] == ']'){
-                currtag[j - 1] = 0;
-            }else{
-                currtag[j - 1] = splitstring[i][j];
+    char **splitstring = split(string, '\n');
+    for (size_t i = 0; splitstring[i]; i++) {
+        if (splitstring[i][0] == '[') {
+            for (int j = 1; j < strlen(splitstring[i]) - 1; j++) {
+                if (splitstring[i][j] == ']') {
+                    currtag[j - 1] = 0;
+                } else {
+                    currtag[j - 1] = splitstring[i][j];
+                }
             }
+
+        } else {
+            struct key_value_pair kvp;
+            for (int j = 0; j < strlen(splitstring[i]); j++) {
+                if (splitstring[i][j] == '=') {
+                    eqindex = j;
+                    break;
+                }
+            }
+            kvp.key = (char *)malloc(strlen(currtag) + eqindex + 2);
+            memcpy(kvp.key, currtag, strlen(currtag));
+            kvp.key[strlen(currtag)] = '.';
+            memcpy(kvp.key + strlen(currtag) + 1, splitstring[i], eqindex);
+            kvp.key[strlen(currtag) + eqindex + 1] = 0;
+
+            kvp.value = (char *)malloc(strlen(splitstring[i]) - eqindex);
+            memcpy(kvp.value, &splitstring[i][eqindex + 1],
+                   strlen(splitstring[i]) - eqindex - 1);
+            kvp.value[strlen(splitstring[i]) - eqindex] = 0;
+
+            vector_push_back(&kvps, kvp);
         }
-        
-       }else
-       {
-        struct key_value_pair kvp;
-            for (int j = 0; j < strlen(splitstring[i]); j++)
-            {
-                 if(splitstring[i][j] == '='){
-                     eqindex = j;
-                     break;
-                 }
-            }
-        kvp.key = (char*)malloc(strlen(currtag) + eqindex + 2);
-        memcpy(kvp.key,currtag,strlen(currtag));
-        kvp.key[strlen(currtag)] = '.';
-        memcpy(kvp.key + strlen(currtag) + 1,splitstring[i],eqindex);
-        kvp.key[strlen(currtag) + eqindex + 1] = 0;
-
-        kvp.value = (char*)malloc(strlen(splitstring[i]) - eqindex);
-        memcpy(kvp.value,&splitstring[i][eqindex + 1],strlen(splitstring[i]) - eqindex - 1);
-        kvp.value[strlen(splitstring[i]) - eqindex] = 0;
-
-        vector_push_back(&kvps,kvp);
-       }
     }
     struct parsed_ini pini;
     pini.count = vector_length(&kvps);
--- parsing/tar.c
+++ parsing/tar.c
@@ -1,71 +1,64 @@
 #ifdef HEAP_ACCESSABLE
 #include "tar.h"
-#include "types/vector.h"
 #include "types/string.h"
-// turns out, the idiots who designed tar, encoded the size into a base 8 string :facepalm:
-uint32_t parse_size(uint8_t *in)
-{
- 
+#include "types/vector.h"
+// turns out, the idiots who designed tar, encoded the size into a base 8 string
+// :facepalm:
+uint32_t parse_size(uint8_t *in) {
+
     uint32_t size = 0;
     uint32_t j;
     uint32_t count = 1;
- 
+
     for (j = 11; j > 0; j--, count *= 8)
         size += ((in[j - 1] - '0') * count);
- 
-    return size;
- 
-}
-vector_static(struct tar_header*,headers);
 
-struct tar_contents parse_tar(struct tar_header* header,uint64_t file_size)
-{
+    return size;
+}
+vector_static(struct tar_header *, headers);
+
+struct tar_contents parse_tar(struct tar_header *header, uint64_t file_size) {
     vector_init(&headers);
 
     uint32_t i = 0;
 
     struct tar_header *_header = header;
- 
-    while(true)
-    {
+
+    while (true) {
         uint64_t traversed_size = (uint64_t)_header - (uint64_t)header;
-        if (_header->filename[0] == 0 || traversed_size >= file_size){
+        if (_header->filename[0] == 0 || traversed_size >= file_size) {
             struct tar_contents contents;
             contents.hdr_num = i;
             contents.headers = headers.data;
             return contents;
         }
- 
+
         uint32_t size = parse_size(_header->size);
- 
-        vector_push_back(&headers,_header);
- 
+
+        vector_push_back(&headers, _header);
+
         _header += ((size / 512) + 1) * 512;
- 
+
         if (size % 512)
             _header += 512;
 
         i++;
     }
 }
-struct tar_header* find_file(struct tar_contents* findin,uint8_t* name)
-{
-    for (size_t i = 0; i < findin->hdr_num; i++)
-    {
-        struct tar_header* hdr = findin->headers[i];
+struct tar_header *find_file(struct tar_contents *findin, uint8_t *name) {
+    for (size_t i = 0; i < findin->hdr_num; i++) {
+        struct tar_header *hdr = findin->headers[i];
         uint8_t right = 0;
-        for (size_t j = 0; j < strlen((char*)name); j++)
-        {
-            if(name[i] == hdr->filename[i]){
+        for (size_t j = 0; j < strlen((char *)name); j++) {
+            if (name[i] == hdr->filename[i]) {
                 right++;
             }
         }
-        if(right == strlen((char*)name)){
+        if (right == strlen((char *)name)) {
             return hdr;
-        } 
+        }
     }
     return 0;
 }
-
 
 #endif
