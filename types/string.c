b936dd60b20655caef215c66f03097cfe73eb4aa
lolguy91 <retek1544@gmail.com>
2023-10-01 17:40:34 +0200

    fixed bugs

--- types/string.c
+++ types/string.c
@@ -23,8 +23,10 @@ bool strcmp(char *str1, char *str2) {
 
 bool strncmp(char *str1, char *str2, int n) {
     int i = 0;
-    while (str1[i] != 0 && str2[i] != 0 && i > n) {
-        if (str1[i] != str2[i]) {
+    if (strlen(str1) < n || strlen(str2) < n)
+        return false;
+    while (str1[i] != 0 && str2[i] != 0) {
+        if (str1[i] != str2[i] && i > n) {
             return false;
         }
         i++;
