9b97d2f0f108a8488b7fd5d2f4011b7f15d4014f
Jozef Nagy <schkwve@gmail.com>
2024-04-22 17:16:10 +0200

    Implemented useful str* functions

--- klibc/string.c
+++ klibc/string.c
@@ -1,3 +1,4 @@
+#include <types/stdtypes.h>
 #include "string.h"
 
 void *memchr(const void *s, int c, size_t n)
@@ -21,15 +22,16 @@ int memcmp(const void *s1, const void *s2, size_t n)
 
 	for (su1 = s1, su2 = s2; n > 0; ++su1, ++su2, --n) {
 		if (*su1 != *su2) {
-			return ((*su1 < *su2) ? -1 : 1);
+			return (*su1 < *su2) ? -1 : 1;
 		}
 	}
 
 	return 0;
 }
 
-void *memcpy(void *s1, const void * s2, size_t n)
+void *memcpy(void *s1, const void *s2, size_t n)
 {
+	// this function assumes the areas do not overlap
 	char *su1;
 	const char *su2;
 
@@ -70,4 +72,134 @@ void *memset(void *s, int c, size_t n)
 	}
 
 	return s;
+}
+
+char *strcat(char *s1, const char *s2)
+{
+	char *s;
+
+	for (s = s1; *s != '\0'; ++s);
+	for (; (*s = *s2) != '\0'; ++s, ++s2);
+
+	return s1;
+}
+
+int strcmp(const char *s1, const char *s2)
+{
+	for (; *s1 == *s2; ++s1, ++s2) {
+		if (*s1 == '\0') {
+			return 0;
+		}
+	}
+	
+	return (*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : 1;
+}
+
+char *strcpy(char *s1, const char *s2)
+{
+	char *s = s1;
+	for (s = s1; (*s++ = *s2++) != '\0';);
+	return s1;
+}
+
+size_t strcspn(const char *s1, const char *s2)
+{
+	const char *sc1, *sc2;
+
+	for (sc1 = s1; *sc1 != '\0'; ++sc1) {
+		for (sc2 = s2; *sc2 != '\0'; ++sc2) {
+			if (*sc1 == *sc2) {
+				return sc1 - s1;
+			}
+		}
+	}
+
+	return sc1 - s1;
+}
+
+size_t strlen(const char *s)
+{
+	const char *sc;
+
+	for (sc = s; *sc != '\0'; ++sc);
+	return sc - s;
+}
+
+char *strncat(char *s1, const char *s2, size_t n)
+{
+	char *s;
+
+	for (s = s1; *s != '\0'; ++s);
+	for (; n > 0 && *s2 != '\0'; --n) {
+		*s++ = *s2++;
+	}
+
+	*s = '\0';
+	return s1;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+	for (; n > 0; ++s1, ++s2, --n) {
+		if (*s1 != *s2) {
+			return (*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : 1;
+		} else if (*s1 == '\0') {
+			return 0;
+		}
+	}
+
+	return 0;
+}
+
+char *strncpy(char *, const char *, size_t)
+{
+	char *s;
+
+	for (s = s1; n > 0 && *s2 != '\0'; --n) {
+		*s++ = *s2++;
+	}
+
+	for (; n > 0; --n) {
+		*s++ = '\0';
+	}
+
+	return s1;
+}
+
+size_t strspn(const char *s1, const char *s2)
+{
+	const char *sc1, *sc2;
+
+	for (sc1 = s1; *sc1 != '\0'; ++sc1) {
+		for (sc2 = s2;; ++sc2) {
+			if (*sc2 == '\0') {
+				return sc1 - s1;
+			} else if (*sc1 == *sc2) {
+				break;
+			}
+		}
+	}
+
+	return sc1 - s1;
+}
+
+char *strtok(char *s1, const char *s2)
+{
+	char *sbegin, *send;
+	static char *ssave = "";
+
+	sbegin = s1 ? s1 : ssave;
+	sbegin += strspn(sbegin, s2);
+	if (*sbegin == '\0') {
+		ssave = "";
+		return NULL;
+	}
+
+	send = sbegin + strcspn(sbegin, s2);
+	if (*send != '\0') {
+		*send++ = '\0';
+	}
+
+	ssave = send;
+	return sbegin;
 }
--- klibc/string.h
+++ klibc/string.h
@@ -8,5 +8,15 @@ int memcmp(const void *, const void *, size_t);
 void *memcpy(void *, const void *, size_t);
 void *memmove(void *, const void *, size_t);
 void *memset(void *, int, size_t);
+char *strcat(char *, const char *);
+int strcmp(const char *, const char *);
+char *strcpy(char *, const char *);
+size_t strcspn(const char*, const char *);
+size_t strlen(const char *);
+char *strncat(char *, const char *, size_t);
+int strncmp(const char *, const char *, size_t);
+char *strncpy(char *, const char *, size_t);
+size_t strspn(const char *, const char *);
+char *strtok(char *, const char *);
 
 #endif // __STRING_H__
