c3619210e4fe6d87053a59576903c1fd288f2311
lolguy91 <retek1544@gmail.com>
2024-04-28 14:25:11 +0000

    a lot of stuffs

--- arch/x86_64/cpu.c
+++ arch/x86_64/cpu.c
@@ -7,14 +7,31 @@ void cpuid(uint32_t eax, uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d) {
 void get_cpu_capabilities(CPUInfo *cpuInfo) {
     uint32_t eax, ebx, ecx, edx;
 
-    cpuid(0, &eax, &ebx, &ecx, &edx);
-
+    //Vendor String
+    cpuid(CPUID_GET_VENDORSTRING, &eax, &ebx, &ecx, &edx);
     memcpy(&cpuInfo->vendor[0], &ebx, 4);
     memcpy(&cpuInfo->vendor[4], &edx, 4);
     memcpy(&cpuInfo->vendor[8], &ecx, 4);
     cpuInfo->vendor[12] = 0;
 
-    cpuid(1, &eax, &ebx, &ecx, &edx);
+    cpuid(CPUID_GET_INTEL_BRANDSTRING1, &eax, &ebx, &ecx, &edx);
+    memcpy(&cpuInfo->brand_string[0],  &eax, 4);
+    memcpy(&cpuInfo->brand_string[4],  &ebx, 4);
+    memcpy(&cpuInfo->brand_string[8],  &ecx, 4);
+    memcpy(&cpuInfo->brand_string[12], &edx, 4);
+    cpuid(CPUID_GET_INTEL_BRANDSTRING2,&eax, &ebx, &ecx, &edx);
+    memcpy(&cpuInfo->brand_string[16], &eax, 4);
+    memcpy(&cpuInfo->brand_string[20], &ebx, 4);
+    memcpy(&cpuInfo->brand_string[24], &ecx, 4);
+    memcpy(&cpuInfo->brand_string[28], &edx, 4);
+    cpuid(CPUID_GET_INTEL_BRANDSTRING3,&eax, &ebx, &ecx, &edx);
+    memcpy(&cpuInfo->brand_string[32], &eax, 4);
+    memcpy(&cpuInfo->brand_string[36], &ebx, 4);
+    memcpy(&cpuInfo->brand_string[40], &ecx, 4);
+    memcpy(&cpuInfo->brand_string[44], &edx, 4);
+    cpuInfo->brand_string[48] = 0;
+
+    cpuid(CPUID_GET_FEATURES, &eax, &ebx, &ecx, &edx);
     cpuInfo->family = (eax >> 8) & 0xF;
     cpuInfo->model = (eax >> 4) & 0xF;
     cpuInfo->stepping = eax & 0xF;
