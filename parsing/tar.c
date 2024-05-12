078b9958e47a09f33c4468899c6df0c63198d01a
lolguy91 <retek1544@gmail.com>
2024-05-12 08:28:01 +0000

    accidentally left the logger in :facepalm

--- parsing/tar.c
+++ parsing/tar.c
@@ -1,7 +1,6 @@
 #ifdef HEAP_ACCESSABLE
 #include "tar.h"
 #include "types/vector.h"
-#include <utils/log.h>
 // turns out, the idiots who designed tar, encoded the size into a base 8 string
 // :facepalm:
 uint32_t parse_size(uint8_t *in) {
